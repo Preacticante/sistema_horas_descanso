c5a524cdde1e5433ecabbd726d69828c47475cc7
Preacticante <practicante.tie@prepauco.edu.mx>
2026-06-30 14:47:34 -0600

    -.

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "http://172.16.6.86:8000";
 
 async function cargarEmpleados(ids = null) {
     const tabla = document.getElementById("tabla-empleados");
